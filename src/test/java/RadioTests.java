b67abfbb5749473005a3406fdf5f2754b6d0349f
Rraaalf <kymyass@gmail.com>
2023-12-10 17:37:18 +0300

    добавлен класс RadioTests

---
+++ src/test/java/RadioTests.java
@@ -0,0 +1,66 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class RadioTests {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/stations/setStations.csv")
+    public void testSetStations(int setStation, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.setCurrentStation(setStation);
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/stations/nextStation.csv")
+    public void testNextStation(int currentStation, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentStation(currentStation);
+
+        radio.nextStation();
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/stations/prevStation.csv")
+    public void testPrevStation(int currentStation, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentStation(currentStation);
+
+        radio.prevStation();
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/volume/incVolume.csv")
+    public void testIncVolume(int setVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(setVolume);
+
+        radio.incVolume();
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/volume/decVolume.csv")
+    public void testDecVolume(int setVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(setVolume);
+
+        radio.decVolume();
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
