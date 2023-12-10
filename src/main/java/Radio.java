782eab40e49194ff903269582a52f002b7985c32
Rraaalf <kymyass@gmail.com>
2023-12-10 17:37:04 +0300

    добавлен класс Radio

---
+++ src/main/java/Radio.java
@@ -0,0 +1,61 @@
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation < 9) {
+            currentStation++;
+        }
+        if (currentStation == 9) {
+            currentStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation--;
+        }
+        if (currentStation == 0) {
+            currentStation = 9;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    /**
+     * Для удобства параметризации автотестов
+     *
+     * @param newCurrentVolume должен быть в пределах от 0 до 100
+     */
+    public void setCurrentVolume(int newCurrentVolume) {
+        currentVolume = newCurrentVolume;
+    }
+
+    public void incVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
+    }
+
+    public void decVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
